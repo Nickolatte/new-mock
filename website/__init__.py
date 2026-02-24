28b389f1555c7dbc0030d903c66c264ac09cc780
Nickolatte <149201093+Nickolatte@users.noreply.github.com>
2026-02-24 13:42:25 +0000

    ver 3
    login and sign up to save

--- website/__init__.py
+++ website/__init__.py
@@ -1,18 +1,38 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
-
-
-
-    
     app.register_blueprint(views, url_prefix='/')
 
+    from .models import User, Booking
+    with app.app_context():
+        db.create_all()
+
+    # Initialize Flask-Login
+    login_manager = LoginManager()
+    login_manager.login_view = 'views.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
 
     return app
+
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
--- website/views.py
+++ website/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint,render_template, request, flash
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db   
+from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
 
@@ -12,11 +15,33 @@ def home():
 def aboutus():
     return render_template('about.html')
 
-@views.route('/login' , methods=['GET', 'POST'])
+@views.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template('login.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
+
+
+@views.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('views.login'))
+
+
 
 @views.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -27,22 +52,35 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash ('Email must be greater than 4 characters.', category='error')
-            pass
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
         elif len(firstName) < 2:
-            flash ('First name must be greater than 2 characters.', category='error')
-            pass
+            flash('First name must be greater than 2 characters.', category='error')
         elif len(lastName) < 1:
-            flash ('Last name must be greater than 1 character.', category='error')    
-            pass
+            flash('Last name must be greater than 1 character.', category='error')    
         elif password1 != password2:
-            flash ('Passwords don\'t match.', category='error')
-            pass
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            flash ('Password must be at least 7 characters.', category='error')
-            pass
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            new_user = User(email=email, first_name=firstName, last_name=lastName, password=generate_password_hash(
+                password1, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(user, remember=True)
+            flash('Account created!', category='success')
+            return redirect(url_for('views.signup'))
+
     return render_template('signup.html')
+
+
+@views.route('/booking')
+@login_required
+def booking():
+    return render_template('booking.html', User=current_user)
 
 
 
